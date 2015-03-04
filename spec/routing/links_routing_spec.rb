711c95867e66d9984c5a3726d19d82738169320d
Nicholas Hamidjaja <nicholas@adskom.com>
2015-03-04 17:01:36 +0700

    Route root post to links#create

--- spec/routing/links_routing_spec.rb
+++ spec/routing/links_routing_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "routes for Links", type: :routing do
   it { expect(get('/')).to route_to('links#new')}
+
+  it { expect(post('/')).to route_to('links#create')}
 end
