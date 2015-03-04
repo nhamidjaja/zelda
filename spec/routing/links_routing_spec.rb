1fb3dca34fb3ef9c180e2e9bc11a69331a71cbfe
Nicholas Hamidjaja <nicholas@adskom.com>
2015-03-04 17:40:48 +0700

    Route for short url redirect

--- spec/routing/links_routing_spec.rb
+++ spec/routing/links_routing_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "routes for Links", type: :routing do
   it { expect(get('/')).to route_to('links#new')}
 
   it { expect(post('/links')).to route_to('links#create')}
-
   it { expect(post: links_path).to route_to('links#create')}
+
+  it { expect(get('/a')).to route_to(controller: 'links', action: 'redirect', short_url: 'a') }
 end
